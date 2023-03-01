62f7b98ec3f44baea4a737dce84fe804ce8efa5b
Maxime Scharwath <maxscharwath@gmail.com>
2023-03-01 11:27:26 +0100

    webrtc

--- src/Main.elm
+++ src/Main.elm
@@ -1,44 +1,125 @@
-module Main exposing (main)
+port module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as D
+
 
 
 -- MAIN
 
+
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+
+-- PORTS
+
+
+port sendMessage : String -> Cmd msg
+port messageReceiver : (String -> msg) -> Sub msg
+
+
 
 -- MODEL
 
-type alias Model = Int
 
-init : Model
-init =
-  0
+type alias Model =
+  { draft : String
+  , messages : List String
+  }
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+  ( { draft = "", messages = [] }
+  , Cmd.none
+  )
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
 
-update : Msg -> Model -> Model
+type Msg
+  = DraftChanged String
+  | Send
+  | Recv String
+
+
+-- Use the `sendMessage` port when someone presses ENTER or clicks
+-- the "Send" button. Check out index.html to see the corresponding
+-- JS where this is piped into a WebSocket.
+--
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    DraftChanged draft ->
+      ( { model | draft = draft }
+      , Cmd.none
+      )
 
-    Decrement ->
-      model - 1
+    Send ->
+      ( { model | draft = "" }
+      , sendMessage model.draft
+      )
+
+    Recv message ->
+      ( { model | messages = model.messages ++ [message] }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+-- Subscribe to the `messageReceiver` port to hear about messages coming in
+-- from JS. Check out the index.html file to see how this is hooked up to a
+-- WebSocket.
+--
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  messageReceiver Recv
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    [ h1 [] [ text "Echo Chat" ]
+    , ul []
+        (List.map (\msg -> li [] [ text msg ]) model.messages)
+    , input
+        [ type_ "text"
+        , placeholder "Draft"
+        , onInput DraftChanged
+        , on "keydown" (ifIsEnter Send)
+        , value model.draft
+        ]
+        []
+    , button [ onClick Send ] [ text "Send" ]
     ]
+
+
+
+-- DETECT ENTER
+
+
+ifIsEnter : msg -> D.Decoder msg
+ifIsEnter msg =
+  D.field "key" D.string
+    |> D.andThen (\key -> if key == "Enter" then D.succeed msg else D.fail "some other key")
