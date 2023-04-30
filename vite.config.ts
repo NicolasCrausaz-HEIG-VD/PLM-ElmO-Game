82c09915a4c1f63798dfb37d8bf04e46c0b43ef1
Maxime Scharwath <maxscharwath@gmail.com>
2023-04-30 11:55:10 +0200

    feat: add PWA support to the application - Add PWA icons and splash screens for various devices - Add a manifest file to the HTML head - Add a theme color to the HTML head - Add meta tags to the HTML head for better SEO - Add a vite-plugin-pwa dependency to the devDependencies in package.json - Ignore .DS_Store files in the .gitignore file
    feat(pwa): add PWA icons and manifest file
    style(elmo.scss): make images responsive
    build(vite.config.ts): add VitePWA plugin to generate PWA manifest and icons
    The commit adds PWA icons and a manifest file to the public/pwa directory. The manifest file contains metadata about the PWA, such as the name, theme color, and icons. The icons are in various sizes and formats to support different devices. The elmo.scss file is updated to make images responsive by setting the max-width to 400px and width to 100%. The vite.config.ts file is updated to include the VitePWA plugin to generate the PWA manifest and icons.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import {plugin as elm} from 'vite-plugin-elm'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isDev = mode === 'development';
 
   return {
-    plugins: [elm({ debug: isDev })],
+    plugins: [
+      VitePWA({ registerType: 'autoUpdate' }),
+      elm({ debug: isDev })
+    ],
   };
 });
